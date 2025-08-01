1d8fa46d225db4fc33cf024b779bc60b75436d79
Vasuki Garg <44336743+Vasuki-Garg@users.noreply.github.com>
2025-08-01 17:15:24 -0400

    Update features.py

--- src/features.py
+++ src/features.py
@@ -9,34 +9,30 @@ import torch
 
 def get_business_features(df):
     features = [
-        'yelp_close9',
-        'yelp_close10',
-        'yelp_avg_rating_moreThan4',
-        'yelp_reviewRating_min_is5',
-        'yelp_avg_rating_lessThan2',
-        'yelp_massageCat',
-        'yelp_business_name_combine',
-        'yelp_spaCat',
-        'yelp_phone_advertisement',
-        'census_pct_manufacturing_industry_low',
-        'landcover_type_developed_high_intensity',
-        'yelp_category_reflexology',
-        'yelp_reviewRating_max_is1',
-        'yelp_lexicon_score_mean_high',
-        'census_pct_nonwhite_high',
-        'census_avg_household_size_high',
-        'min_dist_base_high',
-        'census_pct_20_to_29_low',
-        'yelp_authorGender_pct_male_zero',
-        'yelp_close11',
-        'yelp_authorGender_pct_male_high',
-        'min_dist_police_low',
-        'census_pct_housing_vacant_low',
-        'census_pct_households_with_children_low',
-        'yelp_lexicon_score_mean_zero',
-        'census_pct_over25_with_bachelors_low',
-        'min_dist_base_low',
-        'owner_listed_worker_out_of_state'
+        'yelp_feat1'
+        'yelp_feat2'
+        'yelp_feat3'
+        'yelp_feat4'
+        'yelp_feat5'
+        'yelp_feat6'
+        'yelp_feat7'
+        'yelp_feat8'
+        'yelp_feat9'
+        'yelp_feat10'
+        'yelp_feat11'
+        'yelp_feat12'
+        'licence_feat1'
+        'gis_feat1'
+        'gis_feat2'
+        'gis_feat3'
+        'census_feat1'
+        'census_feat2'
+        'census_feat3'
+        'census_feat4'
+        'census_feat5'
+        'census_feat6'
+        'census_feat7'
+        'landcover_feat1'
     ]
     df_sorted = df.drop_duplicates(subset='bizIdx').sort_values('bizIdx')
     return df_sorted[features].values
@@ -44,11 +40,11 @@ def get_business_features(df):
 
 def get_review_features(df):
     df_sorted = df.drop_duplicates(subset='revIdx').sort_values('revIdx')
-    embeddings = np.array(df_sorted['review_vector'].apply(lambda x: [float(val) for val in x.strip('[]').split()]).tolist())
+    embeddings = np.array(df_sorted['review_feat1'].apply(lambda x: [float(val) for val in x.strip('[]').split()]).tolist())
     pca = PCA(n_components=22)
     reduced_embeddings = pca.fit_transform(embeddings)
-    cols1 = ['roberta_neu', 'roberta_pos', 'roberta_neg']
-    cols2 = ['reviewRating', 'lexicon_score', 'lexicon_prediction']
+    cols1 = ['roberta_feat1', 'roberta_feat2', 'roberta_feat3']
+    cols2 = ['review_feat2', 'lexicon_feat1', 'lexicon_feat2']
     X_review = pd.concat([
         pd.DataFrame(reduced_embeddings),
         df_sorted[cols1].reset_index(drop=True),
@@ -59,7 +55,7 @@ def get_review_features(df):
 
 def get_reviewer_features(df):
     df_sorted = df.drop_duplicates(subset='reviewerIdx').sort_values('reviewerIdx')
-    encoded = pd.get_dummies(df_sorted[['authorGender']], columns=['authorGender'], prefix='gender')
+    encoded = pd.get_dummies(df_sorted[['author_feat2']], columns=['author_feat2'], prefix='gender')
     encoded = encoded.astype(int)
     for i in range(1, 23):
         encoded[f'random_col_{i}'] = np.random.uniform(-0.5, 0.5, len(encoded))
